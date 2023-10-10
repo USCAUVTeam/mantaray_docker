d60a0daccf23cfaab067e6f589e067dfafc835e5
Yamada <Yamada>
2023-10-09 19:45:25 -0700

    Fixing bugs with implementation of auv_ws

---
+++ README.md
@@ -0,0 +1,1 @@
+Run build_image.sh and then run run_image.sh. When build_image.sh asks you for a tag, just type enter to skip.
--- build_image.sh
+++ build_image.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 cd ~/mantaray_docker
+echo "Warning: Default tag for run_image.sh is v0!"
 read -p 'tag: ' tag
 if [[ -z "$tag" ]]; then
-    echo "Setting tag to 0"
-    tag="0"
+    echo "Setting tag to v0"
+    tag="v0"
 fi
 echo "Building to auv:${tag}"
 sudo docker build -t auv:$tag auv/.
